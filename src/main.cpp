f3bade04b13373ebddd125a265d339ed6ed5d16a
ghowoght <2550319354@qq.com>
2022-03-30 15:09:14 +0800

    加入耗时统计的程序片

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,36 @@
 
 inline double rand_mimx(double min, double max){
     auto x = rand();
-
     return min + x / (RAND_MAX * 1.0) * (max - min);
+}
+inline double get_s(clock_t start){
+    return (clock() - (int)start) / (CLOCKS_PER_SEC * 1.0);
 }
 
 int main(int argc, char** argv){
 
     std::vector<point_t> points;
-
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 1000000; i++)
         points.push_back(point_t{rand_mimx(0, 1), rand_mimx(0, 1), rand_mimx(0, 1)});
 
     KDTree kdtree(points);
 
     point_t point({rand_mimx(0, 1), rand_mimx(0, 1), rand_mimx(0, 1)});
 
-    auto nearest = kdtree.get_nearest(point);
-
+    
+    std::cout << "total points: " << points.size() << std::endl;
     std::cout << "input         : " << point[0] << " " << point[1] << " " << point[2] << std::endl;
-    std::cout << "kdtree nearest: " << nearest[0] << " " << nearest[1] << " " << nearest[2] << std::endl;
+    std::cout << "----------- kdtree search -----------" << std::endl;
+    auto startTime = clock();
+    auto nearest = kdtree.get_nearest(point);
+    std::cout << "The run time is: " << get_s(startTime) << "s" << std::endl;
+    std::cout << "nearest: " << nearest[0] << " " << nearest[1] << " " << nearest[2] << std::endl;
+
+    std::cout << "----------- linear search -----------" << std::endl;
+    startTime = clock();
     nearest = kdtree.get_nearest_linear_search(point);
-    std::cout << "kdtree nearest: " << nearest[0] << " " << nearest[1] << " " << nearest[2] << std::endl;
+    std::cout << "The run time is: " << get_s(startTime) << "s" << std::endl;
+    std::cout << "nearest: " << nearest[0] << " " << nearest[1] << " " << nearest[2] << std::endl;
 
     return 0;
 }
