04720edcc9430ecee4e4d20987192d7c5b7f9fe2
木士 鬼 <2550319354@qq.com>
2022-03-31 19:51:33 +0800

    将随机数生成函数改为模板函数

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <algorithm>
 
 
-inline double rand_mimx(double min, double max){
+template<typename T>
+inline T rand_mimx(T min, T max){
     auto x = rand();
     return min + x / (RAND_MAX * 1.0) * (max - min);
 }
@@ -17,7 +18,7 @@ int main(int argc, char** argv){
 
     std::vector<point_t> points;
     for(int i = 0; i < SAMPLE_NUM; i++)
-        points.push_back(point_t{rand_mimx(0, 1), rand_mimx(0, 1), rand_mimx(0, 1)});
+        points.push_back(point_t{rand_mimx<double>(0, 1), rand_mimx<double>(0, 1), rand_mimx<double>(0, 1)});
 
     // 构建KD-Tree
     auto startTime = clock();
@@ -25,7 +26,7 @@ int main(int argc, char** argv){
     std::cout << SAMPLE_NUM << " points' building time is: " << get_s(startTime) << "s" << std::endl;
     
     for(int i = 0; i < INSERT_NUM; i++)
-        points.push_back(point_t{rand_mimx(0, 1), rand_mimx(0, 1), rand_mimx(0, 1)});
+        points.push_back(point_t{rand_mimx<double>(0, 1), rand_mimx<double>(0, 1), rand_mimx<double>(0, 1)});
     // 插入新节点
     startTime = clock();
     std::for_each(points.begin() + SAMPLE_NUM, points.end(), [&](point_t& p){
@@ -34,7 +35,7 @@ int main(int argc, char** argv){
     std::cout << INSERT_NUM << "  points' insert time is  : " << get_s(startTime) << "s" << std::endl;
 
     // 待查询点
-    point_t point({rand_mimx(0, 1), rand_mimx(0, 1), rand_mimx(0, 1)});
+    point_t point({rand_mimx<double>(0, 1), rand_mimx<double>(0, 1), rand_mimx<double>(0, 1)});
     std::cout << "input         : " << point[0] << " " << point[1] << " " << point[2] << std::endl;
     
     // 最近邻搜索
@@ -49,6 +50,7 @@ int main(int argc, char** argv){
     nearest = kdtree.get_nearest_linear_search(point, points);
     std::cout << "The run time is: " << get_s(startTime) << "s" << std::endl;
     std::cout << "nearest: " << nearest[0] << " " << nearest[1] << " " << nearest[2] << std::endl;
-
+    
+    // kdtree.show_tree();
     return 0;
 }
